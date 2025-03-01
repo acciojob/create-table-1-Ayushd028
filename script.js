2f76e0fcbd47650dbbdf5b6bc76185b95a89e868
Ayush Dwivedi <dwivediayush028@gmail.com>
2025-03-01 20:11:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function insert_Row() {
 	let table = document.querySelector("#sampleTable");
 	table.innerHTML = 
 		`
-		<<tr>
+		<tr>
             <td>New Cell1</td>  
             <td>New Cell2</td>
         </tr>
