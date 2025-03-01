2d901c9797658a30e757f8850b8cf002963e5b22
Ayush Dwivedi <dwivediayush028@gmail.com>
2025-03-01 20:21:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
 function insert_Row() {
-
-	let table = document.querySelector("#sampleTable");
-	table.innerHTML = 
-		`
-		<tr>
-            <td>New Cell1</td>  
-            <td>New Cell2</td>
-        </tr>
-		` + table.innerHTML;
-  
+  var table = document.getElementById("sampleTable");
+  table.innerHTML = "<tr><td>New Cell1</td><td>New Cell2</td></tr>" + table.innerHTML;
 }
